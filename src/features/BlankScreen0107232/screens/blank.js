0a538615ee79005970b36bdbaf0e78bb99ff8615
Crowdbotics <team@crowdbotics.com>
2020-09-24 00:57:12 +0000

    Update components

--- src/features/BlankScreen0107232/screens/blank.js
+++ src/features/BlankScreen0107232/screens/blank.js
@@ -31,32 +31,31 @@ export class _Blank extends React.Component {
           "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/download_11.jpg"
       }}
       style={this.props.themedStyle.ImageBackground_1}
-    />
+    >
+      <Image
+        resizeMode="contain"
+        source={{
+          uri:
+            "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/download_13.jpg"
+        }}
+        style={this.props.themedStyle.Image_3}
+      />
+    </ImageBackground>
   )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   ImageBackground_1: {
-    width: 10,
-    height: 20,
-    marginLeft: 3,
-    marginRight: 3,
-    marginTop: 3,
-    marginBottom: 3,
-    paddingLeft: 3,
-    paddingRight: 3,
-    paddingTop: 3,
-    paddingBottom: 3,
     overflow: "scroll",
-    alignSelf: "flex-start",
+    alignSelf: "center",
     fontSize: 18,
     color: "#000000",
     backgroundColor: "#ffffff",
     fontStyle: "normal",
     fontWeight: "normal",
     flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
+    flexWrap: "nowrap",
+    justifyContent: "center",
     alignItems: "stretch",
     alignContent: "stretch",
     borderColor: "#000000",
@@ -68,5 +67,23 @@ export default Blank = withStyles(_Blank, theme => ({
     borderBottomWidth: 0,
     borderRadius: 0,
     backgroundSize: "stretch"
+  },
+  Image_3: {
+    width: 261,
+    height: 200,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 12,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+    overflow: "scroll",
+    alignSelf: "center",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
   }
 }))
