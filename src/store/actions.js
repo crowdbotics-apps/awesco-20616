395ea044dc91866a4b5aa01e0027d2e87df19ee2
Crowdbotics <team@crowdbotics.com>
2020-09-25 17:21:04 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,12 @@
 import * as types from "./constants"
+export const signup_post_api_v1_signup_read = () => ({
+  type: types.SIGNUP_POST_API_V1_SIGNUP_READ
+})
+export const signup_post_api_v1_signup_readSucceeded = response => ({
+  type: types.SIGNUP_POST_API_V1_SIGNUP_READ_SUCCEEDED,
+  response
+})
+export const signup_post_api_v1_signup_readFailed = error => ({
+  type: types.SIGNUP_POST_API_V1_SIGNUP_READ_FAILED,
+  error
+})
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,6 +4,9 @@ const initialState = {}
 
 export default function apiReducer(state = initialState, action) {
   switch (action.type) {
+    case types.SIGNUP_POST_API_V1_SIGNUP_READ:
+    case types.SIGNUP_POST_API_V1_SIGNUP_READ_SUCCEEDED:
+    case types.SIGNUP_POST_API_V1_SIGNUP_READ_FAILED:
     default:
       return state
   }
